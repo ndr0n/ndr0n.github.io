eda26b1a6e2289716dc42470ccd630ebcad5c1e4
ndr0n <58706017+ndr0n@users.noreply.github.com>
2020-01-14 11:11:58 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let size,black;
+let side,size,black;
 
 function preload() {
   black = loadImage("Black.jpg");
@@ -12,24 +12,24 @@ function setup() {
 function draw() {
   size = (windowWidth+windowHeight)/2;
   background(0);
-  let top = (size/32);
+  side = (size/32);
   fill(0);
   stroke(255);
-  rect(0,0,windowWidth, top);
-  rect(0,0,top,windowHeight);
-  rect(0,0,top,top);
+  rect(0,0,windowWidth, side);
+  rect(0,0,side,windowHeight);
+  rect(0,0,side,side);
   fill(255);
   stroke(0);
   textSize(size/64);
-  text("NDR0N",top+(size/32),size/48);
-  image(black, top+size/32, top+(size/32), size/4, size/4);
-  if(mouseX>top+size/32 && mouseX<top+(size/32)+size/4 && mouseY>top+(size/32) && mouseY<top+(size/32)+size/4){
-    text("Black (EP)",top+(size/32)+size/32, top+(size/32)+size/32);
+  text("NDR0N",side+(size/32),size/48);
+  image(black, side+(size/32), side+(size/32), size/4, size/4);
+  if(mouseX>side+size/32 && mouseX<side+(size/32)+size/4 && mouseY>side+(size/32) && mouseY<side+(size/32)+size/4){
+    text("Black (EP)",side+(size/32)+size/32, side+(size/32)+size/32);
   }
 }
 
 function mousePressed(){
-  if(mouseX>top+(size/32) && mouseX<top+(size/32)+size/4 && mouseY>top+(size/32) && mouseY<top+(size/32)+size/4){
+  if(mouseX>side+size/32 && mouseX<side+(size/32)+size/4 && mouseY>side+(size/32) && mouseY<side+(size/32)+size/4){
     window.open("https://tt-ndr0n.bandcamp.com/");
   }
 }
