09fdb58729f354dd63bf33da48525e649cb12de6
ndr0n <58706017+ndr0n@users.noreply.github.com>
2020-01-14 10:44:54 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,10 +12,16 @@ function setup() {
 function draw() {
   size = (windowWidth+windowHeight)/2;
   background(0);
-  image(black, size/32, size/32, size/4, size/4);
-  if(mouseX>size/32 && mouseX<(size/32)+size/4 && mouseY>size/32 && mouseY<(size/32)+size/4){
-    fill(255);
-    text("Black (EP)",(size/32)+size/32, (size/32)+size/32);
+  fill(255);
+  let top = (size/32);
+  rect(0,0,windowWidth, top);
+  fill(0);
+  textSize(size/64);
+  text("NDR0N",size/48,size/48);
+  fill(255);
+  image(black, size/32, top+(size/32), size/4, size/4);
+  if(mouseX>size/32 && mouseX<(size/32)+size/4 && mouseY>top+(size/32) && mouseY<top+(size/32)+size/4){
+    text("Black (EP)",(size/32)+size/32, top+(size/32)+size/32);
   }
 }
 
