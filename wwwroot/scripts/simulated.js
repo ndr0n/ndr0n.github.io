6e6ec6f07ea5db520fde3772d5955b9022e08b80
ndr0n <esquilofreniko@gmail.com>
2024-07-30 22:50:29 +0100

    Fixes

--- wwwroot/scripts/simulated.js
+++ wwwroot/scripts/simulated.js
@@ -4,8 +4,6 @@ window.StrudelInit = () => {
     const strudel = document.createElement('strudel-editor');
     document.getElementById('strudel').append(strudel);
     repl = strudel.editor;
-    // repl.setCode(initcode());
-    // repl.evaluate();
     repl.setCode(restructured());
 };
 
@@ -15,6 +13,10 @@ window.StrudelClear = () => {
 }
 
 window.StrudelPlay = () => {
+    let code = repl.code; 
+    repl.setCode(initcode());
+    repl.evaluate();
+    repl.setCode(code);
     repl.evaluate();
 }
 
@@ -31,33 +33,26 @@ function initcode() {
     return "" +
         "samples('/strudelcycles/simulated.json')\n" +
         "setcps(170/60/4)\n" +
-        "hush()\n" +
         "";
 }
 
 function restructured() {
     return "" +
-        "samples('/strudelcycles/simulated.json')\n" +
-        "setcps(170/60/4)\n" +
-        "\n" +
         "all(x => x.sometimesBy(0.125,y=>y.brak()))\n" +
         "\n" +
-        "d1: stack(\n" +
+        "d1:stack(\n" +
         "s(\"bd\").struct(\"[t ~ ~ t ~ ~ ~ ~]*1\").legato(3).gain(1).cut(1),\n" +
         "s(\"sd\").struct(\"[~ ~ ~ ~ t ~ ~ ~]*1\").legato(0.75).gain(0.7).cut(1),\n" +
         "s(\"hh\").struct(\"[t t t t]*1\").legato(0.125).gain(0.7),\n" +
         "s(\"fh\").struct(\"[~ ~ t ~]*2\").legato(0.25).gain(0.45)\n" +
         ").amp(1)._scope()\n" +
-        "\n" +
-        "d2: stack(\n" +
+        "d2:stack(\n" +
         "s(\"sawtooth*8?\").note(\"[0|2|3|5]*8\").transpose(28).legato(2).gain(1.25).cut(3).lpf(rand.range(25,500)).lpenv(rand.range(1,8)).lpq(15)\n" +
         ").amp(1)._scope()\n" +
-        "\n" +
-        "d3: stack(\n" +
+        "d3:stack(\n" +
         "s(\"vox*8?\").note(24).legato(1).gain(0.6).slice(16, \"[0|1|2|3|4|5|6|7|9|10|11|12|13|14|15]*8\").comb(1)\n" +
         ").amp(1).delay(0.25).delayt(0.33).delayfb(rand.range(0,0.75)).lock(1).room(0.25).roomsize(0.9)._scope()\n" +
-        "\n" +
-        "d4: stack(\n" +
+        "d4:stack(\n" +
         "s(\"glitch*8?0.7\").loopAt(8).legato(1).gain(0.5).slice(8, \"[0|1|2|3|4|5|6|7]*8\")\n" +
         ").amp(1).delay(0.25).delayt(0.33).delayfb(rand.range(0,0.75)).lock(1).room(1).roomsize(0.9)._scope()\n" +
         "";
